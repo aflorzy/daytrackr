ca52fdf5c45899fb5187b129702a81f3645abe87
Andrew <andrewtflores@yahoo.com>
2024-01-16 22:52:48 -0600

    Allow all origins

--- apps/api/src/main/java/com/aflorzy/daytrackr/security/SecurityConfig.java
+++ apps/api/src/main/java/com/aflorzy/daytrackr/security/SecurityConfig.java
@@ -49,7 +49,7 @@ public class SecurityConfig {
   @Bean
   CorsConfigurationSource corsConfigurationSource() {
     CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(List.of("http://localhost:5173", "http://localhost:4200"));
+    configuration.setAllowedOrigins(List.of("*"));
     configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "DELETE"));
     configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
     UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
