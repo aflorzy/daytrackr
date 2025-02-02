e2380d3ffb2f6cff846979037aedf1d23b2e3f53
Andrew <andrewtflores@yahoo.com>
2025-02-02 14:37:29 -0600

    Fix linting issues and converty dirtyCheck to functional guard

--- apps/web/src/app/guards/dirty-check.guard.ts
+++ apps/web/src/app/guards/dirty-check.guard.ts
@@ -1,23 +1,18 @@
-import { Injectable } from "@angular/core";
-
 import { Observable, of, switchMap, take } from "rxjs";
 
 export interface DirtyComponent {
   isDirty$: Observable<boolean>;
 }
 
-@Injectable({ providedIn: "root" })
-export class DirtyCheckGuard  {
-  canDeactivate(component: DirtyComponent) {
-    return component.isDirty$.pipe(
-      switchMap(dirty => {
-        if (dirty === false) {
-          return of(true);
-        }
-        const navigate = confirm("You have unsaved changes. Are you sure you want to leave?");
-        return of(navigate);
-      }),
-      take(1)
-    );
-  }
-}
+export const canDeactivateDirty = (component: DirtyComponent): Observable<boolean> => {
+  return component.isDirty$.pipe(
+    switchMap(dirty => {
+      if (!dirty) return of(true);
+
+      const navigate = confirm("You have unsaved changes. Are you sure you want to leave?");
+
+      return of(navigate);
+    }),
+    take(1)
+  );
+};
