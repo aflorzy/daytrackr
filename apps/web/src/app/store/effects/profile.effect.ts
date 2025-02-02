bf500479ae45e8fbab40f4b8fa56ee878893bd01
Andrew <andrewtflores@yahoo.com>
2025-02-02 15:00:36 -0600

    Update packages

--- apps/web/src/app/store/effects/profile.effect.ts
+++ apps/web/src/app/store/effects/profile.effect.ts
@@ -1,5 +1,6 @@
 import { inject, Injectable } from "@angular/core";
-import { Actions, concatLatestFrom, createEffect, ofType } from "@ngrx/effects";
+import { Actions, createEffect, ofType } from '@ngrx/effects';
+import { concatLatestFrom } from '@ngrx/operators';
 import { Store } from "@ngrx/store";
 import { catchError, delay, map, of, switchMap } from "rxjs";
 import { StatusType } from "src/app/enums";
